9270595401f74530fd676f1ced0f98dfcfecc83d
vincesaykao <vtsaykao@hotmail.com>
2022-02-20 21:00:32 -0600

    added router POST

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,8 +1,37 @@
 const express = require('express');
+const pool = require('../../../group-fs-react-shopping-list/server/modules/pool');
 const router = express.Router();
 const galleryItems = require('../modules/gallery.data');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
+
+
+// POST Route
+router.post('/', (req,res) => {
+    const sqlText = `
+    INSERT INTO "gallery" ("path","description","likes")
+    VALUES ($1,$2,$3);    
+    `
+    const { path, description, likes } = req.body;
+
+    if (path == '' || description == '' || likes == '') {
+        res.sendStatus(999);
+        console.log('Undefined data sent to server');
+        return;
+    }
+    console.log( { path, description, likes } );
+    const sqlOptions = [path,description,likes];
+
+    pool.query(sqlText,sqlOptions)
+    .then((response) => {
+        console.log('Success from server POST Route', response);
+        res.sendStatus(201);
+    })
+    .catch((error) => {
+        console.log('error POST', error);
+        res.sendStatus(500);
+    })
+}); // END POST Route
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
